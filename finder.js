1d711220e7880157158af5d472397db0d259b021
injecti0n <rootgebruiker@gmail.com>
2020-02-18 21:08:47 +0100

    updated

--- finder.js
+++ finder.js
@@ -1,60 +1,68 @@
-  var placeSearch, autocomplete;
-  var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
-  };
+
+(function ($, window, document) {
   
-  function initAutocomplete() {
-    // Create the autocomplete object, restricting the search to geographical
-    // location types.
-    autocomplete = new google.maps.places.Autocomplete(
-        (document.getElementById('autocomplete')),
-        {types: ['geocode']});
-  
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
-  }
-  
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-    console.log(componentForm);
-  
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-    }
-  
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    console.log(place);
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
-    }
-  }
-  
-  // Bias the autocomplete object to the user's geographical location,
-  // as supplied by the browser's 'navigator.geolocation' object.
-  function geolocate() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var geolocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        var circle = new google.maps.Circle({
-          center: geolocation,
-          radius: position.coords.accuracy
+  var address = {
+      init : function(){
+        var self = this;
+
+        var $autocomplete = $('#autocomplete');
+        self.component_form = {
+            street_number: 'short_name',
+            route: 'long_name',
+            locality: 'long_name',
+            administrative_area_level_1: 'short_name',
+            country: 'long_name',
+            postal_code: 'short_name'
+          };
+
+        //
+        self.autocomplete = new google.maps.places.Autocomplete(($autocomplete[0]),{ types: ['geocode', 'establishment'] });
+
+        self.events();
+
+       
+
+      },
+    
+      events : function(){
+        var self = this;
+        
+        $('#form-submit').on('click', function(e){
+          var l = Ladda.create(this);
+  	 	l.start();
         });
-        autocomplete.setBounds(circle.getBounds());
-      });
-    }
-  }
+        
+       
+        self.l = Ladda.create($('.button-search')[0]);
+
+        $('input#autocomplete').on('keyup', function() {
+      	 	self.l.start();       
+        });
+        google.maps.event.addListener(self.autocomplete, 'place_changed', function() { 
+            var place = self.autocomplete.getPlace();
+            
+            	
+            
+            // kill all input values
+            $('.text-inputs .form-control').val('');
+            
+            
+            for (var i = 0; i < place.address_components.length; i++) {
+                var address_type = place.address_components[i].types[0];
+                if (self.component_form[address_type]) {
+                  var val = place.address_components[i][self.component_form[address_type]];
+                  
+                  $('.text-inputs .' + address_type).val(val);
+                  
+                }
+              }
+          
+         console.log(place);
+            self.l.stop();
+          });
+      }
+   }
+
+  address.init();
+
+})(jQuery, window, document);
